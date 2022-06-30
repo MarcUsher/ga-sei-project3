3b3983cfaeeaadc19d4bb13cc639f56f321324da
marc <marcusher@outlook.com>
2022-06-30 16:24:37 +0100

    Pair programming with Richard (rakitent) on completing styling and layout for Cake Details page, inc. recipe add pop-up modal

--- cakeproject/settings.py
+++ cakeproject/settings.py
@@ -94,7 +94,7 @@ DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.postgresql',
         'NAME': os.getenv('NAME'),
-        'USER': os.getenv('USER'),
+        'USER': os.getenv('USERNAME'),
         'PASSWORD': os.getenv('PASSWORD')
     }
 }
